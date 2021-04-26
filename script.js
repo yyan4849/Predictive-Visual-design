db8f3d8328d989684656f4eb9c8c5bb3a7c70c83
yyan4849 <yyan4849@uni.sydney.edu.au>
2021-04-26 10:33:42 +0800

    feat:add plotly graphics

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ if (window.pageYOffset >= bannerTop){
     sidebar.style.display = 'none';
 }
 })
+
+// make a plot
+
+myDiv = document.getElementById('myDiv');
+function make_plot(csv_data){
+    //Filter our csv data for a particular country
+    //Try logging country_data to the console to see what's in it
+    let country_data = csv_data.filter(d => d.country == "China");
+
+    //Add our main data trace
+    let data = [{
+        x: country_data.map(d => d.year),
+        y: country_data.map(d => d.mortality),
+        mode: 'lines'
+    }]
+
+    //Draw the plot at our div
+    Plotly.newPlot(myDiv, data);
+}
+
+//Load the csv data and when loaded: run the make_plot function with that data
+Plotly.d3.csv("mortality.csv", make_plot);
+
+
+
